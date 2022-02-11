7a9f0e3ab2ba9cea37620ffc6ee72a6f1b676205
ntnyq <ntnyq@users.noreply.github.com>
2022-02-11 10:13:48 +0000

    deploy: 4ba7e4aa98e5ab364d8da03e8eb84d3ba4f693e3

--- static/js/184.55e7cb6a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkelement_plus_admin"]=self["webpackChunkelement_plus_admin"]||[]).push([[184],{3184:function(e,t,r){r.r(t),r.d(t,{default:function(){return p}});var u=r(6788),n=r(4356),a={name:"TheRedirect",setup(){const{route:e,router:t}=(0,n.j)(),{params:r,query:a}=e,{path:s}=r;return t.replace({path:`/${s}`,query:a}),()=>(0,u.h)("div")}};const s=a;var p=s}}]);
